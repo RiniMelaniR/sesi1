775a01ee9498cae00d5eca265b7b229d67759499
RiniMelaniR <91415207+RiniMelaniR@users.noreply.github.com>
2021-09-26 18:08:29 +0700

    Add files via upload
    Rini Melani R
    20200040080
    TI20E

---
+++ tugas1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesi1;
+
+/**
+ *
+ * @Rini Melani R
+ */
+public class tugas1 {
+    public static void main(String[] args) {
+        System.out.println("Belajat JAVA");
+        System.out.println("Sangat Mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+    
+}
---
+++ tugas2.java
@@ -0,0 +1,18 @@
+package sesi1;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @Rini Melani R
+ */
+public class tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String pertanyaan = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+pertanyaan+" Sangat mudah dan menyenangkan");
+    }
+    
+}
